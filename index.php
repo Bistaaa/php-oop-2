2b8545ce17679725383d1086c151ed7c0ecfdba4
Bistaaa <michele.bistarelli.mb@outlook.it>
2023-07-09 21:23:38 +0200

    creazione card

--- index.php
+++ index.php
@@ -1,136 +1,61 @@
-<?php
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>E-commerce</title>
 
-class Category {
-    private $icon;
-    private $name;
+    <?php require_once("./data/db.php"); ?>
 
-    public function __construct($icon, $name) {
-        $this->setIcon($icon);
-        $this->setName($name);
-    }
+</head>
+<body>
+    
+    <h1>Food</h1>
 
-    public function getIcon() {
-        return $this->icon;
-    }
+    <?php
 
-    public function setIcon($icon) {
-        $this->icon = $icon;
-    }
+        foreach ($foods as $food) {
+            
+            ?> 
+            
+                <div>
+                    <h3>
+                        [<?php echo $food -> getCategory() -> getName(); ?>]
+                        <?php echo $food -> getName(); ?>
+                    </h3>
+                    <div>Peso: <?php echo $food -> getWeight() ?>Kg</div>
+                    <div>Prezzo: <?php echo $food -> getPrice() ?>E</div>
+                    <div>Data scadenza: <?php echo $food -> getExpireDate() ?></div>
+                </div>
+            
+            <?php
+        }
 
-    public function getName() {
-        return $this->name;
-    }
+    ?>
 
-    public function setName($name) {
-        $this->name = $name;
-    }
-}
+    <h1> Toys </h1>
 
-class Product {
-    private $name;
-    private $weight;
-    private $price;
-    private $category;
+    <?php
 
-    public function __construct($name, $weight, $price, Category $category) {
-        $this->setName($name);
-        $this->setWeight($weight);
-        $this->setPrice($price);
-        $this->setCategory($category);
-    }
+        foreach ($toys as $toy) {
+            
+            ?> 
+            
+                <div>
+                    <h3>
+                        [<?php echo $toy -> getCategory() -> getName(); ?>]
+                        <?php echo $toy -> getName(); ?>
+                    </h3>
+                    <div>Peso: <?php echo $toy -> getWeight() ?>Kg</div>
+                    <div>Prezzo: <?php echo $toy -> getPrice() ?>E</div>
+                    <div>Colore: <?php echo $toy -> getColor() ?></div>
+                    <div>Eta' consigliata: <?php echo $toy -> getAge() ?></div>
+                </div>
+            
+            <?php
+        }
 
-    public function getName() {
-        return $this->name;
-    }
+    ?>
 
-    public function setName($name) {
-        $this->name = $name;
-    }
-
-    public function getWeight() {
-        return $this->weight;
-    }
-
-    public function setWeight($weight) {
-        $this->weight = $weight;
-    }
-
-    public function getPrice() {
-        return $this->price;
-    }
-
-    public function setPrice($price) {
-        $this->price = $price;
-    }
-
-    public function getCategory() {
-        return $this->category;
-    }
-
-    public function setCategory($category) {
-        $this->category = $category;
-    }
-}
-
-class Food extends Product {
-    private $expireDate;
-
-    public function __construct($name, $weight, $price, Category $category, $expireDate) {
-        parent:: __construct($name, $weight, $price, $category);
-        $this->setExpireDate($expireDate);
-    }
-
-    public function getExpireDate() {
-        return $this->expireDate;
-    }
-
-    public function setExpireDate($expireDate) {
-        $this->expireDate = $expireDate;
-    }
-}
-
-class Toy extends Product {
-    private $color;
-    private $age;
-
-    public function __construct ($name, $weight, $price, Category $category, $color, $age) {
-        parent:: __construct($name, $weight, $price, $category);
-        $this->setColor($color);
-        $this->setAge($age);
-    }
-
-    public function getColor() {
-        return $this->color;
-    }
-
-    public function setColor($expcolorireDate) {
-        $this->color = $color;
-    }
-
-    public function getAge() {
-        return $this->age;
-    }
-
-    public function setAge($age) {
-        $this->age = $age;
-    }
-}
-
-$dogCategory = new Category("dog", "cane");
-$catCategory = new Category("cat", "gatto");
-var_dump($dogCategory);
-
-echo "<br>----------------------<br>";
-
-$p1 = new Product("p1", 5, 20, $dogCategory);
-var_dump($p1);
-
-echo "<br>----------------------<br>";
-
-$f1 = new Food("Cibo cane", 5, 30, $dogCategory, "10/10/2023");
-var_dump($f1);
-
-echo "<br>----------------------<br>";
-
-$t1 = new Toy("Toy1", 1, 20, $catCategory, "green", "1-3");
-var_dump($t1);
+</body>
+</html>
---
+++ models/Toy.php
@@ -0,0 +1,28 @@
+<?php
+
+class Toy extends Product {
+    private $color;
+    private $age;
+
+    public function __construct ($name, $weight, $price, Category $category, $color, $age) {
+        parent:: __construct($name, $weight, $price, $category);
+        $this->setColor($color);
+        $this->setAge($age);
+    }
+
+    public function getColor() {
+        return $this->color;
+    }
+
+    public function setColor($expcolorireDate) {
+        $this->color = $color;
+    }
+
+    public function getAge() {
+        return $this->age;
+    }
+
+    public function setAge($age) {
+        $this->age = $age;
+    }
+}
